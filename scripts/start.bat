ba0cd441ce2f9f48de8b69db59f18d3fee7c61e4
pshuvalov <frament@mail.ru>
2017-11-21 23:33:35 +0500

    Фикс скриптов для запуска

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,4 +1,4 @@
 cd ../parse-configs
 start /b cmd /c mongodb-runner start
-start /b cmd /c parse-server ./parse-configs/server.json
-start /b cmd /c parse-dashboard --config ./parse-configs/dashboard.json
+start /b cmd /c parse-server ./server.json
+start /b cmd /c parse-dashboard --config ./dashboard.json
